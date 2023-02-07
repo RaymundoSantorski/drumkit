48218661831f61a3013224f389f5002f667850cb
Raymundo Mateos <rayma9829@gmail.com>
2023-02-07 15:40:48 -0600

    [ADD] Se agrega la clase playing al div contenedor de la tecla, al estar activa

--- public/index.js
+++ public/index.js
@@ -1,6 +1,16 @@
+let keys = document.querySelectorAll('div.key');
+
 document.addEventListener('keydown', (e) => {
     let audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     if(!audio) return;
     audio.currentTime = 0;
     audio.play();
+    let key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    key.classList.add('playing');
+});
+
+keys.forEach(key => {
+    key.addEventListener('transitionend', (e) => {
+        console.log(e);
+    }); 
 });
